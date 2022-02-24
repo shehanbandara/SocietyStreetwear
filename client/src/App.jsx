7ad665dc83ff07099fbad046bf3d3c27a70a606f
shehanbandara <s2bandara@uwaterloo.ca>
2022-02-24 17:37:05 -0500

    Navigation Bar
    Navigation Bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
+import Home from "./pages/Home";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <Home />
+  );
 };
 
 export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavigationBar from '../components/NavigationBar';
+
+const Home = () => {
+    return (
+        <div>
+            <NavigationBar />
+        </div>
+    );
+};
+
+export default Home;
