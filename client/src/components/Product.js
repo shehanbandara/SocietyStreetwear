4fa52d121657b7a614a61b38e18bc7a408d4b740
shehanbandara <s2bandara@uwaterloo.ca>
2022-02-26 23:44:53 -0500

    Finished product boilerplate
    Finished product boilerplate

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom';
 const Product = () => {
     return (
         <div className='Product'>
-            <img src='https://www.nicekicks.com/files/2021/11/A-Ma-Maniere-Air-Jordan-1-High-OG-DO7097-100-Lead2.jpg' alt='Jordan 1 A Ma Maniere' />
-            <div className='Product-info'>
-                <p className='Product-info-name'>Jordan 1 A Ma Maniere</p>
-                <p className='Product-info-size'>Size: 11</p>
-                <p className='Product-info-condition'>Condition: 10/10 Deadstock</p>
-                <p className='Product-info-price'>Price: $600</p>
-                <Link to={`/product/${1234}`} className='Product-info-button'>View Product</Link>
-            </div>
+            <Link to={`/product/${1234}`} className='Product-link'>
+                <img src='https://www.nicekicks.com/files/2021/11/A-Ma-Maniere-Air-Jordan-1-High-OG-DO7097-100-Lead2.jpg' alt='Jordan 1 A Ma Maniere' />
+                <div className='Product-info'>
+                    <p className='Product-info-name'>Jordan 1 A Ma Maniere</p>
+                    <p className='Product-info-size'>Size: 11</p>
+                    <p className='Product-info-condition'>Condition: 10/10 Deadstock</p>
+                    <p className='Product-info-price'>Price: $600</p>
+                </div>
+            </Link>
         </div>
     );
 };
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -7,6 +7,11 @@ const HomePage = () => {
             <h2 className='HomePage-title'>Products</h2>
             <div className='HomePage-products'>
                 <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
             </div>
         </div>
     );
